0c083c0e3afa37afb6e7c2c164a6a20e2ad66aae
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-10-23 10:13:03 -0300

    CSS nos componentes de layout

--- moments/src/app/app.routes.ts
+++ moments/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'moments/new', component: NewMomentComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
